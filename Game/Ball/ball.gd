ff04ce186a9a10d7034e59542854d7d0dba3f6a5
SecretShinobi <marc.lunar@mail.utoronto.ca>
2024-10-09 01:13:27 -0400

    setting up game
    setting up the base of the breakout game

--- Game/Ball/ball.gd
+++ Game/Ball/ball.gd
@@ -8,7 +8,7 @@ extends CharacterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	set_velocity(Vector2(-500.0, 0.0))
+	set_velocity(Vector2(0.0, -500.0))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,15 +18,15 @@ func _physics_process(delta):
 		velocity = velocity.bounce(collision.get_normal())
 		# Change the bounce angle if ball hits paddle based on dist. from center
 		if collision.get_collider() is CharacterBody2D:
-			var ball_y: float = get_global_position().y
-			var paddle_y: float = collision.get_collider().get_global_position().y
+			var ball_x: float = get_global_position().x
+			var paddle_x: float = collision.get_collider().get_global_position().x
 			#velocity.x *= 1.1 # Increase speed for each bounce
-			velocity.y = (ball_y - paddle_y) * 5
+			velocity.x = (ball_x - paddle_x) * 5
 
 
 func reset_ball():
 	set_position(Vector2(0.0, 0.0))
-	set_velocity(Vector2(velocity.x, 0.0))
+	set_velocity(Vector2(velocity.y, 0.0))
 
 
 # Signal Methods

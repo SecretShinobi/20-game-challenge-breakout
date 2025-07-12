2b53be831f9bf6f382a7c24e44d69d0276ae6383
SecretShinobi <marc.lunar@mail.utoronto.ca>
2025-07-11 22:34:03 -0400

    Update game.gd

--- Game/game.gd
+++ Game/game.gd
@@ -18,15 +18,16 @@ var bricks_remaining: int = 0
 func _ready():
 	ball.life_lost.connect(_on_life_lost)
 	
-	var spawn_point = Vector2(400.0, 200.0)
+	var start_point = Vector2(400.0, 200.0)
 	
-	for i in range(0, 10): # Spawn in bricks
-		var brick_instance = brick.instantiate()
-		brick_instance.scored_points.connect(_on_scored_points)
-		add_child(brick_instance)
-		bricks_remaining += 1
-		brick_instance.set_position(spawn_point)
-		spawn_point.x += 120
+	for i in range(0, 5): # Spawn in bricks
+		for j in range(0, 9):
+			var spawn_point = Vector2(start_point.x + (120 * j), start_point.y + (50 * i)) 
+			var brick_instance = brick.instantiate()
+			brick_instance.scored_points.connect(_on_scored_points)
+			add_child(brick_instance)
+			bricks_remaining += 1
+			brick_instance.set_position(spawn_point)
 
 
 func _on_scored_points(points: int):
